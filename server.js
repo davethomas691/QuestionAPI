ebfb538c91a90b283bc92bfd8ccdfc53cc35ccab
David Thomas <davidthomas@Davids-Mac-Pro.local>
2015-08-03 19:01:14 -0400

    S3
    S3

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var express    = require('express');        // call express
 var app        = express();               // define our app using express
 var bodyParser = require('body-parser');
 var aws = require('aws-sdk');
-var AwsS3Form = require( "aws-s3-form" );
 
 var http = require("http");
 var url = require("url");
